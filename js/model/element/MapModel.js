b5da4dfb8d57043184e48df9d66890e1528200cc
Meathill <meathill@gmail.com>
2012-06-07 09:11:19 +0800

    修改grid1（未完成）
    Signed-off-by: Meathill <meathill@gmail.com>

--- js/model/element/MapModel.js
+++
@@ -1,9 +0,0 @@
-jQuery.namespace('Meatazine.model.element');
-Meatazine.model.element.MapModel = Backbone.Model.extend({
-  defaults: {
-    lat: 39.904214,
-    lng: 116.407413,
-    zoom: 15,
-    markers: null,
-  },
-});
