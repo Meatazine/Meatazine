ea7670246be3b239943d9fba6cda9a2c23890a4f
Meathill <meathill@gmail.com>
2012-06-10 15:50:27 +0800

    将 FileReferrence.js 回滚到上一版本，这种充满回调的功能不适合用prototype扩展
    Signed-off-by: Meathill <meathill@gmail.com>

--- js/filesystem/FileReferrence.js
+++ js/filesystem/FileReferrence.js
@@ -1,154 +1,151 @@
 jQuery.namespace('Meatazine.filesystem');
-(function (ns) {
-  var fileSystem;
-  ns.FileReferrence = function () {
-    var self = this;
-    this.reader = new FileReader();
-    this.targetFile;
-    this.fileDir;
-    this.fileName;
-    this.fileType;
-    this.fileContent;
-    this.fileURL;
-    this.folders;
-    this.params;
-    
-    _.extend(this, Backbone.Events);
-    this.reader.onloadend = function (event) {
-      self.trigger('complete:read', event.target.result);
+Meatazine.filesystem.FileReferrence = function () {
+  var self = this,
+      reader = new FileReader(),
+      targetFile,
+      fileDir,
+      fileName,
+      fileType,
+      fileSystem,
+      fileContent,
+      fileURL,
+      folders,
+      params;
+  this.clone = function (file, dir, filename) {
+    if (file == null) {
+      throw new Error('文件错误');
     }
-    window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
-    window.webkitRequestFileSystem(TEMPORARY, 128 * 1024 * 1024, this.fileSystemReadyHandler, this.errorHandler);
-  }
-  ns.FileReferrence.prototype = {
-    constructor: Meatazine.filesystem.FileReferrence,
-    clone: function (file, dir, filename) {
-      if (file == null) {
-        throw new Error('文件错误');
-      }
-      if (file.fileSize > 10 * 1024 * 1024) {
-        throw new Error('不能使用超过10M的素材');
-      }
-      if (!(/image/gi).test(file.type)) {
-        throw new Error('只能上传图片类素材');
-      }
-      this.targetFile = file;
-      this.fileDir = dir;
-      this.fileName = this.filename || file.name;
-      this.fileName = dir ? dir + '/' + this.fileName : this.fileName;
-      if (dir) {
-        this.on('complete:createDirs', function () {
-          this.off('complate:createDirs');
-          fileSystem.root.getFile(this.fileName, {create: true}, this.fileEntry_cloneReadyHandler, this.errorHandler);
-        }, this);
-        this.folders = dir.split('/');
-        this.createDir(fileSystem.root);
-      } else {
-        fileSystem.root.getFile(this.fileName, {create: true}, this.fileEntry_cloneReadyHandler, this.errorHandler);
-      }
-    },
-    copy: function (file, dir, filename, args) {
-      this.on('complete:read', function (data) {
-        this.save(filename, dir, data, 'image/jpeg', args);
+    if (file.fileSize > 10 * 1024 * 1024) {
+      throw new Error('不能使用超过10M的素材');
+    }
+    if (!(/image/gi).test(file.type)) {
+      throw new Error('只能上传图片类素材');
+    }
+    targetFile = file;
+    fileDir = dir;
+    fileName = filename || file.name;
+    fileName = fileDir ? fileDir + '/' + fileName : fileName;
+    if (fileDir) {
+      this.on('complete:createDirs', function () {
+        this.off('complate:createDirs');
+        fileSystem.root.getFile(fileName, {create: true}, fileEntry_cloneReadyHandler, errorHandler);
       }, this);
-      this.read(file);
-    },
-    read: function (url) {
-      window.webkitResolveLocalFileSystemURL(url, this.fileEntry_readReadyHandler);
-    },
-    save: function (name, dir, content, type, args) {
-      this.fileDir = dir;
-      this.fileName = dir ? fileDir + '/' + name : name;
-      this.fileContent = content;
-      this.fileType = type || 'text/plain';
-      this.params = args;
-      if (dir) {
-        this.on('complete:createDirs', function () {
-          this.off('complate:createDirs');
-          fileSystem.root.getFile(this.fileName, {create: true, exclusive: true}, this.fileEntry_saveReadyHandler, this.errorHandler);
-        }, this);
-        this.folders = dir.split('/');
-        this.createDir(fileSystem.root);
-      } else {
-        fileSystem.root.getFile(this.fileName, {create: true, exclusive: true}, this.fileEntry_saveReadyHandler, this.errorHandler);
-      }
-    },
-    createDir: function (root) {
-      if (this.folders[0] == '.' || this.folders[0] == '') {
-        this.folders = folders.slice(1);
-      }
-      root.getDirectory(this.folders.shift(), {create: true}, this.dirEntry_readerHandler, this.errorHandler);
-    },
-    dirEntry_readerHandler: function (dirEntry) {
-      if (this.folders.length > 0) {
-        this.createDir(dirEntry);
-      } else {
-        this.trigger('complete:createDirs');
-      }
-    },
-    fileSystemReadyHandler: function (fs) {
-      fileSystem = fs;
-    },
-    fileEntry_cloneReadyHandler: function (fileEntry) {
-      this.fileURL = fileEntry.toURL();
-      fileEntry.createWriter(this.fileWriter_cloneReadyHandler, this.errorHandler);
-    },
-    fileEntry_readReadyHandler: function (fileEntry) {
-      fileEntry.file(this.fileReadyHandler, this.errorHandler);
-    },
-    fileEntry_saveReadyHandler: function (fileEntry) {
-      this.fileURL = fileEntry.toURL();
-      fileEntry.createWriter(this.fileWriter_saveReadyHandler, this.errorHandler);
-    },
-    fileEntry_removeReadyHandler: function (fileEntry) {
-      fileEntry.remove(this.fileRemoveHandler, this.errorHandler);
-    },
-    fileWriter_cloneReadyHandler: function (fileWriter) {
-      fileWriter.onwriteend = function(event) {
-        console.log('Clone completed.', this.fileURL);
-        this.trigger('complete:clone', this.fileURL);
-      };
-      fileWriter.onerror = function(error) {
-        console.log('Clone failed: ' + e.toString());
-      };
-      fileWriter.write(this.targetFile);
-      this.targetFile = null;
-    },
-    fileWriter_saveReadyHandler: function (fileWriter) {
-      fileWriter.onwriteend = function (event) {
-        console.log('Save completed.', this.fileURL);
-        this.trigger('complete:save', this.fileURL, this.params);
-      };
-      fileWriter.onerror = function (error) {
-        console.log('Save failed: ' + error.toString());
-      };
-      
-      // 处理二进制数据
-      var builder = new WebKitBlobBuilder();
-      if ((/text/i).test(this.fileType)) {
-        builder.append(this.fileContent);
-      } else {
-        var byteArray = new Uint8Array(this.fileContent.length);
-        for (var i = 0, len = this.fileContent.length; i < len; i++) {
-          byteArray[i] = this.fileContent.charCodeAt(i) & 0xFF;
-        }
-        builder.append(byteArray.buffer);
-      }
-      fileWriter.write(builder.getBlob(this.fileType));
-      this.fileContent = null;
-    },
-    fileReadyHandler: function (file) {
-      this.reader.readAsBinaryString(file);
-    },
-    fileRemoveHandler: function () {
-      console.log('Removed: ' + this.fileName);
-      this.save(this.fileName, this.fileDir, this.fileContent, this.fileType, this.params);
-    },
-    errorHandler: function (error) {
-      console.log('Error: ' + error.code, error);
-      if (error.code == FileError.INVALID_MODIFICATION_ERR) {
-        fileSystem.root.getFile(fileName, {create: false}, fileEntry_removeReadyHandler, errorHandler);
-      }
+      folders = dir.split('/');
+      createDir(fileSystem.root);
+    } else {
+      fileSystem.root.getFile(fileName, {create: true}, fileEntry_cloneReadyHandler, errorHandler);
     }
   }
-})(Meatazine.filesystem);
+  this.copy = function (file, dir, filename, args) {
+    this.on('complete:read', function (data) {
+      this.save(filename, dir, data, 'image/jpeg', args);
+      this.off('complete:read', null, this);
+    }, this);
+    this.read(file);
+  }
+  this.read = function (url) {
+    window.webkitResolveLocalFileSystemURL(url, fileEntry_readReadyHandler);
+  }
+  this.save = function (name, dir, content, type, argus) {
+    fileDir = dir;
+    fileName = fileDir ? fileDir + '/' + name : name;
+    fileContent = content;
+    fileType = type || 'text/plain';
+    params = argus;
+    if (fileDir) {
+      this.on('complete:createDirs', function () {
+        this.off('complate:createDirs');
+        fileSystem.root.getFile(fileName, {create: true, exclusive: true}, fileEntry_saveReadyHandler, errorHandler);
+      }, this);
+      folders = dir.split('/');
+      createDir(fileSystem.root);
+    } else {
+      fileSystem.root.getFile(fileName, {create: true, exclusive: true}, fileEntry_saveReadyHandler, errorHandler);
+    }
+  }
+  function createDir(root) {
+    if (folders[0] == '.' || folders[0] == '') {
+      folders = folders.slice(1);
+    }
+    root.getDirectory(folders[0], {create: true}, dirEntry_readerHandler, errorHandler);
+  }
+  function dirEntry_readerHandler(dirEntry) {
+    folders.shift();
+    if (folders.length > 0) {
+      createDir(dirEntry);
+    } else {
+      self.trigger('complete:createDirs');
+    }
+  }
+  function fileSystemReadyHandler(fs) {
+    fileSystem = fs;
+  }
+  function fileEntry_cloneReadyHandler(fileEntry) {
+    fileURL = fileEntry.toURL();
+    fileEntry.createWriter(fileWriter_cloneReadyHandler, errorHandler);
+  }
+  function fileEntry_readReadyHandler(fileEntry) {
+    fileEntry.file(fileReadyHandler, errorHandler);
+  }
+  function fileEntry_saveReadyHandler(fileEntry) {
+    fileURL = fileEntry.toURL();
+    fileEntry.createWriter(fileWriter_saveReadyHandler, errorHandler);
+  }
+  function fileEntry_removeReadyHandler(fileEntry) {
+    fileEntry.remove(fileRemoveHandler, errorHandler);
+  }
+  function fileWriter_cloneReadyHandler(fileWriter) {
+    fileWriter.onwriteend = function(e) {
+      console.log('Clone completed.', fileURL);
+      self.trigger('complete:clone', fileURL);
+    };
+    fileWriter.onerror = function(e) {
+      console.log('Clone failed: ' + e.toString());
+    };
+    fileWriter.write(targetFile);
+    targetFile = null;
+  }
+  function fileWriter_saveReadyHandler(fileWriter) {
+    fileWriter.onwriteend = function (event) {
+      console.log('Save completed.', fileURL);
+      self.trigger('complete:save', fileURL, params);
+    };
+    fileWriter.onerror = function (error) {
+      console.log('Save failed: ' + error.toString());
+    };
+    
+    // 处理二进制数据
+    var builder = new WebKitBlobBuilder();
+    if (fileType.indexOf('text') == -1) {
+      var byteArray = new Uint8Array(fileContent.length);
+      for (var i = 0, len = fileContent.length; i < len; i++) {
+        byteArray[i] = fileContent.charCodeAt(i) & 0xFF;
+      }
+      builder.append(byteArray.buffer);
+    } else {
+      builder.append(fileContent);
+    }
+    fileWriter.write(builder.getBlob(fileType));
+    fileContent = null;
+  }
+  function fileReadyHandler(file) {
+    reader.readAsBinaryString(file);
+  }
+  function fileRemoveHandler() {
+    console.log('Removed: ' + fileName);
+    self.save(fileName, fileDir, fileContent, fileType, params);
+  }
+  function errorHandler(error) {
+    console.log('Error: ' + error.code, error);
+    if (error.code == FileError.INVALID_MODIFICATION_ERR) {
+      fileSystem.root.getFile(fileName, {create: false}, fileEntry_removeReadyHandler, errorHandler);
+    }
+  }
+  _.extend(this, Backbone.Events);
+  reader.onloadend = function (event) {
+    self.trigger('complete:read', event.target.result);
+  }
+  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+  window.webkitRequestFileSystem(TEMPORARY, 128 * 1024 * 1024, fileSystemReadyHandler, errorHandler)
+}
+Meatazine.utils.fileAPI = new Meatazine.utils.FileReferrence();
