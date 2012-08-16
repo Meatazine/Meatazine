4349da94a73d4e53c1c6da73d1ea0e61b4458d45
Meathill <meathill@gmail.com>
2012-08-16 09:10:32 +0800

    没动多少
    Signed-off-by: Meathill <meathill@gmail.com>

--- js/service/AssetsSyncService.js
+++ js/service/AssetsSyncService.js
@@ -1,7 +1,17 @@
 jQuery.namespace('Meatazine.service');
 (function (ns) {
-  var init = {
-    
-  }
+  var queue = [],
+      uploaded = [],
+      init = {
+        add: function (asset) {
+          
+        },
+        remove: function (asset) {
+          
+        },
+        next: function () {
+          
+        }
+      }
   ns.AssetsSyncService = _.extend(init, Backbone.Events);
 })(Meatazine.service);
