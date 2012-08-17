e6fa14c21c19604cc87d83b831ca64ba954b680d
Meathill <meathill@gmail.com>
2012-08-17 09:08:06 +0800

    升级测试用例，看看本地文件能否读取属性，以及能否直接绑在ajax上传
    Signed-off-by: Meathill <meathill@gmail.com>

--- js/service/AssetsSyncService.js
+++ js/service/AssetsSyncService.js
@@ -2,16 +2,16 @@ jQuery.namespace('Meatazine.service');
 (function (ns) {
   var queue = [],
       uploaded = [],
-      init = {
-        add: function (asset) {
-          
-        },
-        remove: function (asset) {
-          
-        },
-        next: function () {
-          
-        }
-      }
-  ns.AssetsSyncService = _.extend(init, Backbone.Events);
+      isUploading = false;
+  ns.AssetsSyncService = {
+    add: function (asset) {
+      
+    },
+    remove: function (asset) {
+      
+    },
+    next: function () {
+      
+    }
+  }
 })(Meatazine.service);
