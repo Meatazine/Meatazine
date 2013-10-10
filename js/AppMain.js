19355fc04fab0a3d988741086aefd1a6f903ba82
Meathill <meathill@gmail.com>
2013-10-11 00:59:46 +0800

    使用bower进行包管理
    Signed-off-by: Meathill <meathill@gmail.com>

--- js/AppMain.js
+++ js/AppMain.js
@@ -67,4 +67,16 @@
   
   // 检查登录状态
   user.checkLoginStatus();
+
+  // load ga
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36011684-1']);
+  _gaq.push(['_setDomainName', 'evereditor.com']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 });
--- libs/jquery.namespace.js
+++
@@ -1,9 +0,0 @@
-Nervenet.createNameSpace = function () {
-  for (var a = null, c, d = 0; d < arguments.length; d += 1) {
-    c = arguments[d].split(".");
-    for (var a = window, b = 0; b < c.length; b += 1) {
-      a[c[b]] = a[c[b]] || {}, a = a[c[b]]
-    }
-  }
-  return a
-};
